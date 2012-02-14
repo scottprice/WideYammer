216aa5d6bd0d40685fa4b5336e3a1a7a0b45febb
Scott Price <scottprice1@gmail.com>
2012-02-14 13:35:55 -0500

    Added source files.

---
+++ wideYammer.js
@@ -0,0 +1,50 @@
+
+(function (wideYammer, $, undefined) {
+	wideYammer.padding = 100;
+
+	wideYammer.resize = function () {	
+		if ($(".three-column-layout").length > 0) {
+			if ($(window).width() >= 1080)
+			{
+				$('.three-column-layout').css('width', $(window).width() - parseInt(wideYammer.padding,10));
+				$('#column-two').css('width', $(window).width() - (parseInt(wideYammer.padding,10) + 480) );
+			}	
+			else {
+				$('.three-column-layout').css('width', 980 );
+				$('#column-two').css('width', 500 );			
+			}
+		}
+		else if ($(".column-two-right").length > 0) {
+			if ($(window).width() >= 1080) {				
+				$('.two-column-layout').css('width', $(window).width() - parseInt(wideYammer.padding,10));
+				$('#column-two').css('width', $(window).width()- (parseInt(wideYammer.padding,10)+ 200) );	
+				$('.column-two-left').css('width', $(window).width() - (parseInt(wideYammer.padding,10)+ 500) );		
+			}	
+			else {			
+				$('.two-column-layout').css('width', 980 );
+				$('#column-two').css('width', 782 );		
+				$('.column-two-left').css('width', 500 );			
+			}		
+		}
+	};
+}(window.wideYammer = window.wideYammer || {}, jQuery));
+
+chrome.extension.sendRequest({method: "getLocalStorage", key: "paddingSize"}, function(response) {	
+	if (response.data && !isNaN(response.data)) {
+		wideYammer.padding = response.data;	
+	}		
+	wideYammer.resize();	
+});
+
+$(window).resize(function () {	
+	wideYammer.resize();
+});
+
+$(document).bind('DOMNodeInserted', function (event) {
+      $(".ymodule-instance tbody h6").css('width','100%');
+	  $(".ymodule-instance tbody p ").css('width','100%');
+	  $(".ymodule-instance tbody p ").css('display','block');
+	  $(".ymodule-instance tbody h7 ").css('width','100%');
+	  $(".ymodule-instance tbody h7 ").css('display','block');
+});
+
